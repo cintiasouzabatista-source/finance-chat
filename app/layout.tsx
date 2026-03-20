026ebec63aaf6c9b8cede3c4abffec32b261eae5
cintiasouzabatista-source <cintiasouzabatista@gmail.com>
2026-03-20 10:22:17 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
-export default function RootLayout({ children }) {
+import React from "react";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html>
+    <html lang="pt-br">
       <body>{children}</body>
     </html>
   );
